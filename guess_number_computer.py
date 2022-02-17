86aaf602344b5d3af8b5af7517e19a4e04bb9bb5
NataliaSzymczyk <nataliaszymczyk0@gmail.com>
2022-02-17 10:44:04 +0100

    guess_v2.0

--- guess_number_computer.py
+++ guess_number_computer.py
@@ -1,22 +1,26 @@
 def guess_2():
     """Gra w zgadywanie. Komputer odgaduje liczbę użytkownika w 10 próbach."""
     game_min = 0
-    game_max = 1000
+    game_max = 1001
     print('Pomyśl liczbę od 0 do 1000, a komputer spróbuje ją zgadnąć w maksymalnie 10 próbach!')
     i=0
     cheat_detector=[]
     while i != 10:
         guess=int((game_max-game_min)/2+game_min)
         print(f'Zgaduję: {guess}')
-        cheat_detector.append(guess)
+        if guess not in cheat_detector:
+            cheat_detector.append(guess)
+        else:
+            return "Nie oszukuj mnie! To powinna być uczciwa gra!"
+
         try:
             chosen_option = int(input(
-"""
-wybierz jedną z opcji:
-'za mała liczba' - 1
-'za duża liczba' - 2
-'odgadnięta liczba' - 3
-"""))
+                                        """
+                                        wybierz jedną z opcji:
+                                        'za mała liczba' - 1
+                                        'za duża liczba' - 2
+                                        'odgadnięta liczba' - 3
+                                        """))
         except ValueError:
             print('Nie mogę tego przekształcić w liczbę całkowitą. Zacznij od nowa.')
             break
@@ -32,8 +36,6 @@ wybierz jedną z opcji:
         i += 1
 
     else:
-        if any(cheat_detector.count(elem) != 1 for elem in cheat_detector):
-            return 'Oszukiwałes!'  # sposób ten sprawdzi jedynie oczywiste oszustwo
         print(f'Próby zgadywania: {cheat_detector}')
         return 'Przegrana. Gramy jeszcze raz?'
 
