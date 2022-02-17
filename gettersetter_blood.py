bc59258692204cce73c9a545721df06b9906a97a
NataliaSzymczyk <nataliaszymczyk0@gmail.com>
2022-02-17 18:47:59 +0100

    gettersetter

--- gettersetter_blood.py
+++ gettersetter_blood.py
@@ -1,18 +1,18 @@
 class HumanBlood:
-    """ getter & setter - enable setting only specific blood types. """
+    """ getter & setter - enables setting only specific blood types. """
 
     def __init__(self, blood_type='Not specified'):
-        self.blood_type = blood_type
+        self.__blood_type = blood_type
 
     def __str__(self):
-        return f'obiekt {self.__class__.__name__}'
+        return f'Obiekt {self.__class__.__name__} o grupie krwi "{self.__blood_type}".'
 
     @property
-    def show_blood_type(self):
+    def blood(self):
         return self.__blood_type
 
-    @show_blood_type.setter
-    def show_blood_type(self, blood_type):
+    @blood.setter
+    def blood(self, blood_type):
         if blood_type in ['A+', 'B+', 'AB+', 'O+', 'A-', 'B-', 'AB-', 'O-']:
             self.__blood_type = blood_type
         else:
@@ -20,21 +20,23 @@ class HumanBlood:
 
 
 
-
 if __name__ == '__main__':
-
     human1 = HumanBlood()
+    human2 = HumanBlood()
     print(human1)
-    print(human1.blood_type)
-    human1.show_blood_type = 'B-'
-    print(human1.show_blood_type)
+    human1.blood='B-'
+    print(human1)
+    print(human1.blood)
     print(human1._HumanBlood__blood_type) # name mangling
+    human2.blood='A+'
+    print(human2)
 
-    # human2 = HumanBlood()
-    # print(human2)
-    # print(human2.blood_type)
-    # human2.show_blood_type = 'X'
-    # print(human2.show_blood_type)
+
+    # PROGRAM NIE POZWOLI NA ZAPIS BŁĘDNEJ GRUPY KRWI
+    # human3 = HumanBlood()
+    # human3.blood='X-'
+    # print(human3)
+
 
 
 
